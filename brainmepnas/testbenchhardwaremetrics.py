bd88864337f19aa2eb84ed79cbacc06745fcd3ec
Jonathan Larochelle <larochelle@kvm.imtek>
2024-05-21 16:07:30 +0200

    TestbenchHardwareMetrics now does a check for outliers in the measured values.

--- brainmepnas/testbenchhardwaremetrics.py
+++ brainmepnas/testbenchhardwaremetrics.py
@@ -2,9 +2,13 @@
 
 # import built-in module
 import dataclasses
+import pathlib
+import warnings
+from typing import Optional, Union
 
 # import third-party modules
 import numpy as np
+import pandas as pd
 try:
     from seizuredetectiontestbench import RemoteTestbench, NNResults
 except ImportError:
@@ -45,7 +49,8 @@ class TestbenchHardwareMetrics(HardwareMetrics):
     nnresults: "NNResults"
 
     def __init__(self, host: str, user: str, password: str,
-                 tflite_model_path: str):
+                 tflite_model_path: Union[str, pathlib.Path],
+                 output_dir: Optional[Union[str, pathlib.Path]] = None):
         """
         Estimate the hardware performance of a TFLite model from the
         Seizure Detection Testbench.
@@ -65,14 +70,58 @@ class TestbenchHardwareMetrics(HardwareMetrics):
             Password of remote testbench for user in plaintext. Make sure to
             never write the password in any file which could be made public
             through external or public versioning tools.
-        tflite_model_path: str
+        tflite_model_path: Union[str, pathlib.Path]
             Path to .tflite file.
+        output_dir: Union[str, pathlib.Path], optional
+            Directory to store output traces from the remote testbench.
+
+        Raises
+        ------
+        RuntimeError
+            If the measured values are all outliers compared to the expected
+            inference duration.
         """
+        if isinstance(tflite_model_path, str):
+            tflite_model_path = pathlib.Path(tflite_model_path)
+        if output_dir is not None:
+            if isinstance(output_dir, str):
+                output_dir = pathlib.Path(output_dir)
+
         rt = RemoteTestbench(host, user, password)
         rt.begin_test_tflite_model(tflite_model_path)
 
-        self.nnresults = rt.get_results_test_tflite_model()
-        self.inference_energy = np.mean(self.nnresults.total_energy)
-        self.inference_time = np.mean(self.nnresults.total_duration)
-        self.memory_ram = self.nnresults.memory_ram
-        self.memory_flash = self.nnresults.memory_flash
+        nnresults = rt.get_results_test_tflite_model(str(output_dir.resolve()))
+
+        # The testbench returns values for a certain number of inferences.
+        # Due to instabilities with the testbench itself (which should be
+        # eventually corrected), some inferences randomly run badly,
+        # leading to outlier energy and/or duration measurements.
+
+        # A true reference is the duration measured on the microcontroller and
+        # outputed directly to the iostream.
+        #   These logs are stored in mcu_iostream_parsed.csv
+        energy_measurement_log_path = output_dir / "mcu_iostream_parsed.csv"
+        df_mcu_iostream = pd.read_csv(energy_measurement_log_path)
+        expected_execution_time = df_mcu_iostream[df_mcu_iostream["layer_name"] == "ALL"][
+            "execution_time"].mean()
+
+        # We expect real measured durations to be within +- 10% of
+        # expected_execution_time.
+        idx_to_ignore = []
+        for id, duration in enumerate(nnresults.total_duration):
+            if not 0.9 * expected_execution_time < duration < 1.1 * expected_execution_time:
+                idx_to_ignore.append(id)
+        if len(idx_to_ignore) == nnresults.nb_repetitions:
+            raise RuntimeError("All iterations were excluded.")
+        elif len(idx_to_ignore) > 0:
+            warnings.warn(f"{len(idx_to_ignore)} iterations were excluded "
+                          f"due to unexpectedly long or short inference time.")
+
+        energy_values_subset = np.delete(nnresults.total_energy, idx_to_ignore)
+        duration_values_subset = np.delete(nnresults.total_duration, idx_to_ignore)
+
+        self.inference_energy = np.mean(energy_values_subset)
+        self.inference_time = np.mean(duration_values_subset)
+        self.memory_ram = nnresults.memory_ram
+        self.memory_flash = nnresults.memory_flash
+        self.nnresults = nnresults
--- tests/test_testbenchhardwaremetrics.py
+++ tests/test_testbenchhardwaremetrics.py
@@ -2,6 +2,8 @@
 
 # import built-in module
 import configparser
+import pathlib
+import tempfile
 
 # import third-party modules
 import pytest
@@ -14,13 +16,16 @@ class TestTestbenchHardwareMetrics:
     """
     Create a copy of the file "remotetestbench_EXAMPLE.ini" under the name
     "remotetestbench.ini" and input the connection settings.
+
+    Note: tests must be run with -s flag to avoid conflicts between pytest and
+    the fabric.runner.Runner.
     """
     REMOTE_TESTBENCH_CONFIG = "remotetestbench.ini"
 
     # .tflite model comes from audio_example1 from Silicon Labs' Machine
     # Learning Toolkit.
     # Link: https://github.com/SiliconLabs/mltk/blob/master/mltk/models/examples/audio_example1.mltk.zip
-    GOOD_TFLITE_MODEL = "audio_example1.tflite"
+    GOOD_TFLITE_MODEL = pathlib.Path("audio_example1.tflite")
 
     # TODO: Consider mocking RemoteTestbench.
 
@@ -34,16 +39,18 @@ class TestTestbenchHardwareMetrics:
         """
         Appropriate results are obtained for a valid tflite model.
         """
-        hm = TestbenchHardwareMetrics(host=remote_testbench_config["credentials"]["host"],
-                                      user=remote_testbench_config["credentials"]["user"],
-                                      password=remote_testbench_config["credentials"]["password"],
-                                      tflite_model_path=self.GOOD_TFLITE_MODEL)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            hm = TestbenchHardwareMetrics(host=remote_testbench_config["credentials"]["host"],
+                                          user=remote_testbench_config["credentials"]["user"],
+                                          password=remote_testbench_config["credentials"]["password"],
+                                          tflite_model_path=self.GOOD_TFLITE_MODEL,
+                                          output_dir=pathlib.Path(temp_dir))
 
-        assert 0 < hm.inference_energy < 0.001
-        assert 0 < hm.inference_time < 1
-        assert 0 < hm.memory_ram < 256000
-        assert 0 < hm.memory_flash < 1500000
-        assert hm.nnresults is not None
+            assert 0 < hm.inference_energy < 0.001
+            assert 0 < hm.inference_time < 1
+            assert 0 < hm.memory_ram < 256000
+            assert 0 < hm.memory_flash < 1500000
+            assert hm.nnresults is not None
 
     def test_bad_tflite_model(self, remote_testbench_config):
         """
