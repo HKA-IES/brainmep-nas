44c8b0d87f102a689bb4b88a64a578899b883f78
jonathan-larochelle <jonathan.larochelle@imtek.uni-freiburg.de>
2024-05-27 13:08:49 +0200

    tflite model conversion now works with tensorflow 2.16

--- brainmepnas/tf_utils.py
+++ brainmepnas/tf_utils.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # import built-in module
-from typing import Literal
+import tempfile
+from typing import Literal, Optional
 
 # import third-party modules
 import tensorflow as tf
@@ -11,17 +12,28 @@ import numpy as np
 
 
 def generate_tflite_model(keras_model: tf.keras.Model,
-                          representative_input: np.ndarray,
                           input_format: Literal["float", "int8"],
-                          output_format: Literal["float", "int8"]):
+                          output_format: Literal["float", "int8"],
+                          representative_input: Optional[np.ndarray] = None):
     """
-    Convert the given keras model to a tflite model with a randomly generated
-    representative dataset.
+    Convert the given keras model to a tflite model.
 
-    :param keras_model: Keras model to convert.
-    :param input_format: One of float or int8.
-    :param output_format: One of float or int8.
-    :return tflite_model
+    Parameters
+    ----------
+    keras_model: tf.keras.Model
+        Keras model to convert.
+    input_format: Literal["float", "int8"]
+        Format of the input to the keras model.
+    output_format: Literal["float", "int8"]
+        Format of the output of the keras model.
+    representative_input: np.ndarray, optional
+        Representative input data, used to perform the quantization. If not
+        given, quantization is performed using randomly generated data between
+        -1 and 1 (float) or -128 and 127 (int8).
+
+    Returns
+    -------
+    tflite_model: tf.keras.Model
     """
     if input_format == "float":
         input_tensor_type = tf.float32
@@ -41,29 +53,36 @@ def generate_tflite_model(keras_model: tf.keras.Model,
     else:
         raise ValueError(f"output_format={output_format} is not supported.")
 
+    keras_model.build()
+
     if representative_input is None:
         def representative_dataset():
             nb_samples = 100
             min_value = representative_dataset_min
             max_value = representative_dataset_max
-            input_shape = keras_model.layers[0].input_shape[1:]
+            input_shape = keras_model.input_shape[1:]
             for i in range(nb_samples):
                 data = (max_value - min_value) * np.random.random_sample(input_shape) + min_value
                 data = data.astype(np.float32)
                 yield [np.expand_dims(data, axis=0)]
     else:
         def representative_dataset():
-            input_shape = keras_model.layers[0].input_shape[1:]
             for x in representative_input:
                 yield [np.expand_dims(x, axis=[0, 3]).astype(
                 np.float32)]
 
-    converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
-    converter.optimizations = [tf.lite.Optimize.DEFAULT]  # Should always be set to [tf.lite.Optimize.DEFAULT]
-    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]  # Should always be set to [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-    converter.inference_input_type = input_tensor_type  # Either tf.float32, tf.int8 (recommended), tf.uint8
-    converter.inference_output_type = output_tensor_type  # Either tf.float32, tf.int8 (recommended), tf.uint8
-    converter.representative_dataset = representative_dataset
-    tflite_model = converter.convert()
+    # Bug: tensorflow 2.16.1
+    # converter.convert() raises AttributeError, see https://github.com/tensorflow/tensorflow/issues/63867
+    # Fix is to use save model.
+    # converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        keras_model.export(tmp_dir)
+        converter = tf.lite.TFLiteConverter.from_saved_model(tmp_dir)
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]  # Should always be set to [tf.lite.Optimize.DEFAULT]
+        converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]  # Should always be set to [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+        converter.inference_input_type = input_tensor_type  # Either tf.float32, tf.int8 (recommended), tf.uint8
+        converter.inference_output_type = output_tensor_type  # Either tf.float32, tf.int8 (recommended), tf.uint8
+        converter.representative_dataset = representative_dataset
+        tflite_model = converter.convert()
 
     return tflite_model
